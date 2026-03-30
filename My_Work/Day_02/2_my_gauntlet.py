457f1496db63b7aa4b9fc93c66db72c81498e475
AGIQdev-Aditya <agiq.dev@gmail.com>
2026-03-30 16:57:13 +0530

    day 1 audited and fixed - all issues resolved

--- My_Work/Day_02/2_my_gauntlet.py
+++ My_Work/Day_02/2_my_gauntlet.py
@@ -10,4 +10,82 @@
 name = 'Aditya'
 print('names first letter: ', name[0], 'names last letter: ', name[-1])
 
-# sorry going to sleep right will come stronger tomorrow morning
+# sorry going to sleep right will come stronger tomorrow morning  
+
+# am back 
+
+# P2
+
+world = 'STARK'
+a = len(world) // 2
+print(world[a])
+
+# P3
+
+cmd = "   reboot_system_now   "
+d = cmd.strip().upper()
+
+# P4    
+
+log = "system_ok: memory_stable"
+print('ERROR' in log)
+
+# P4.5
+
+data = "     X-1      "
+print(data.strip()[0])
+print(data[0])
+
+# got it 
+
+# P5
+
+reading = "velocity=25.5m/s"
+print(reading[-6:-1])
+
+# p6
+
+ELEMENT = 'magnesium'
+print(ELEMENT[0:3:2].upper())
+
+# P7
+
+a = 'AGI_CORE_BETA'
+print(a[::-1])
+
+# P8
+
+c = 'system_update.tar.gz'
+print(c.replace('.tar.gz', ''))
+
+# P9
+
+d = 'Server = https://mirror.rackspace.com/archlinux/$repo/os/$arch'
+start_index = d.find('//') + 2
+end_index = d.find('/', start_index)
+print(d[start_index:end_index])
+
+# p10
+
+rp = 'ID:007|MODULE:KERNEL|TEMP:24.85'
+
+# 1. Find the Module (Start after "MODULE:", end at the next "|")
+mod_start = rp.find('MODULE:') + 7
+mod_end = rp.find('|', mod_start)
+module_name = rp[mod_start:mod_end].lower() # Only call lower() once!
+
+# 2. Find the Temperature (Start after "TEMP:" and go to the end)
+temp_start = rp.find('TEMP:') + 5
+temperature = rp[temp_start:]
+
+print('module:', module_name + ',', 'temp:', temperature)
+
+# p11
+
+a = 'http://insecure.com'
+b = 'https://stark-industries.com'
+c = 'ftp://files.org'
+
+print(a.startswith('https') and a.endswith('.com'))
+print(b.startswith('https') and b.endswith('.com'))
+print(c.startswith('https') and c.endswith('.com'))
--- My_Work/Day_02/3_my_forge.py
+++ My_Work/Day_02/3_my_forge.py
@@ -3,3 +3,60 @@
 #  Build every project from 3_forge.py here.
 #  Make something real.
 # ═══════════════════════════════════
+
+
+# so here I start
+
+# is still don't know how to take input but will try
+
+raw_log = input("Enter Log: ").strip()
+print('so your log is:', raw_log.strip())
+
+module = raw_log[raw_log.find('[') + 1 : raw_log.find(']')]
+print('Module is:', module)
+
+e = print('if there is error:', 'ERROR' in raw_log)
+w = print('if there is warning:', 'WARNING' in raw_log)
+i = print('if there is info:', 'INFO' in raw_log)
+
+# pretty much done  
+# i haven't yet learned input function and if else elif stuff 
+
+
+
+# so heres teh second one of the day 2 teaching 
+
+message = input("Message: ")
+
+reversed_msg = message[::-1]
+
+first = reversed_msg[0]
+last = reversed_msg[-1]
+middle = reversed_msg[1:-1]
+
+salt = '::ENCRYPTED::'
+
+scrembled_rev_msg = last + middle + first + salt
+
+print(scrembled_rev_msg)
+
+
+# done thsi one was more eassy i guess 
+
+# now for decryption 
+
+removed_mark = scrembled_rev_msg.replace(salt, '')
+
+r_first = removed_mark[-1]
+
+r_last = removed_mark[0]
+
+decrypted_msg = r_first + middle + r_last
+
+re_revewrsed_msg = decrypted_msg[::-1]
+
+print('decrypted msg is:', re_revewrsed_msg)
+
+
+
+
