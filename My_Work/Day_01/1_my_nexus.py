84d791f0ac3d557c19ac4807446ca5db094ddd5d
Aditya Agnihotri <agiq.dev@gmail.com>
2026-08-11 02:39:31 +0530

    dpoen something some things

--- My_Work/Day_01/1_my_nexus.py
+++ My_Work/Day_01/1_my_nexus.py
@@ -11,99 +11,109 @@
 print("so here i start")
 
 print("systemonline. Welcome, Aditya!")
-print("dont mind i am just riting what i am able to see on that etachinig filess  liek what its saying to that it if something feels off okay")
+print(
+    "dont mind i am just riting what i am able to see on that etachinig filess  liek what its saying to that it if something feels off okay"
+)
 
 print("this works ")
-print('this also works')
+print("this also works")
 
-print('loading assets...')
-print('establishing neural link ha ha')
-print('j a r v i s  is online fellows')
+print("loading assets...")
+print("establishing neural link ha ha")
+print("j a r v i s  is online fellows")
 
-print('temperature sensor reading:')
+print("temperature sensor reading:")
 print(28.5)
 
-print('success rate is not that much for thsi persion to life on with his wifes money that she is earning from her job as a doctor')
+print(
+    "success rate is not that much for thsi persion to life on with his wifes money that she is earning from her job as a doctor"
+)
 
-print('hey bud how are you doing', 'i think you might have', 5, 'bucks to be precise ', 'my dogs want to fill his car tank up so that it can go to drive' )
+print(
+    "hey bud how are you doing",
+    "i think you might have",
+    5,
+    "bucks to be precise ",
+    "my dogs want to fill his car tank up so that it can go to drive",
+)
 
-print('line one')
+print("line one")
 print()
-print('line three')
+print("line three")
 
-print('home', 'xyz', 'python self learning', sep='/')
-print('processing.....', end='')
-print('completed.')
+print("home", "xyz", "python self learning", sep="/")
+print("processing.....", end="")
+print("completed.")
 
-#so the prediction 1 is core voltage 1.2 v
-# prediction 2 is ststus: ok | system: armed
+# so the prediction 1 is core voltage 1.2 v
+# prediction 2 is status: ok | system: armed
 
 print("Core", "Voltage", 1.2, "V")
 
 print("STATUS: OK", end=" | ")
 print("SYSTEM: ARMED")
 
-print('---------------------------')
+print("---------------------------")
 
-mission_name = 'hulahu ha ha  kuch bhi'
+mission_name = "hulahu ha ha  kuch bhi"
 duration = 10
 
-print('starting mission:')
+print("starting mission:")
 print(mission_name)
-print('estimated duration:')
-print(duration, 'minutes')
+print("estimated duration:")
+print(duration, "minutes")
 
 ini_velo = 10
 acceleration = 9.8
 time = 10
 final_velo = ini_velo + (acceleration * time)
 
-print('final velo after 10s of free fall:')
+print("final velo after 10s of free fall:")
 print(final_velo)
 pin_a = 1
 pin_b = 2
-print('pin_a:', pin_a, 'pin_b:', pin_b)
+print("pin_a:", pin_a, "pin_b:", pin_b)
 
 hello = 0
-print('hello:', hello)
+print("hello:", hello)
 hello = hello + 1
-print('hello:', hello)
+print("hello:", hello)
 hello = hello + 1
-print('hello:', hello)
+print("hello:", hello)
 
 hi = 0.9 * 10
-hello = 0.8 *10
-
-hi = hello
-hello = hi
-
-print('hi:', 'is now', hi)
-print('hello:', 'is now', hello)
+hello = 0.8 * 10
 
 
-print('==================================')
+hi, hello = hello, hi
+
+print("hi:", "is now", hi)
+print("hello:", "is now", hello)
+
+
+print("==================================")
 
 a = 1
 b = 1.2
-c = 'oyee bhava'
+c = "oyee bhava"
 d = True
 
-print('a=', a)
-print('b=', b)
+print("a=", a)
+print("b=", b)
 print("c=", c)
-print('d=', d)
+print("d=", d)
 
 # new learning huh i was not knowing this before
-print('xey =' + 'x*10^y')
+print("xey =" + "x*10^y")
 # also
-print('^=' + '**')
+print("^=" + "**")
 
 # like 10 to the power of 100  which looks like  10^100 but to solve it in python we will write 10**100
 
-print('10^100 =', 10**100)
+print("10^100 =", 10**100)
 
 # now
-a =10
+a = 10
 b = 20.20
 c = "hello"
 d = True
@@ -112,11 +122,10 @@ print(type(a), type(b), type(c), type(d))
 
 
 a = 10
-b = 4/1
-c = 4/2
+b = 4 / 1
+c = 4 / 2
 
 print(a, b, c)
 print(type(a), type(b), type(c))
 
-# yup i got it correct 
-
+# yup i got it correct
