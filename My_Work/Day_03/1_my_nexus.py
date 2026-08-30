a38cad246806ab8b2f9e41a0f9b98b95e9cf5740
Aditya Agnihotri <agiq.dev@gmail.com>
2026-08-30 22:25:35 +0530

    day 3 nexus is doen   thsi one didnt make me to wriet too muxh  but may be teh concept was not that  tought  or becasue i  used a different model and way   that whyy  will see what we cna doo aboput it

--- My_Work/Day_03/1_my_nexus.py
+++ My_Work/Day_03/1_my_nexus.py
@@ -3,3 +3,37 @@
 #  Code along with 1_nexus.py here.
 #  Read one section. Type it yourself. Never copy-paste.
 # ═══════════════════════════════════
+
+
+# 1.  because the input always returns a string that way it returns "42"
+
+# 2. because  a string cant be4 qadded to a number thats why it returns an error
+
+# 3. ONE IS STRING AND THE OTHER ONE IS AN INTEGER
+
+# 4. because the target  at first got the old value of the mission whicyh was moon thats why
+
+
+# also i need to take care as thsi one was was not making me to like write and all that stufff yet  will see if it does later    or i have to change  the prompts and  teh way to checking and  making thnsi thing
+
+
+print(input("What is your name?"))
+
+
+# 6
+
+# 1.   actually dont klnw will  see now  by searching   so  it just taht et rpyhton  is just looking  for   +- or  number btw 1 to 9  and nothing more that weha
+
+#  same dont know  will check now      it because  python is looking for thing and  tyhing  already exists  and if it dioes not get that .  and number after that it automaticvally ataches a .0  in that end
+
+#   the diffrerent is that i guess that athe string  is just attaching both teh strings together and teh    intergeer is actually ddoing the addition   as they are integers
+
+#   i guess because teh txt score is a string and we cant  liek add a sting and a integer
+
+
+negative_text = "-3"
+negative_number = int(negative_text)
+print("negative_text:", negative_text)
+print("negative_number:", negative_number)
+print("type(negative_number):", type(negative_number))
+print()
